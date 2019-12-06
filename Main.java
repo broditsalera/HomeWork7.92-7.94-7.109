6d0048d0550a38c769eb80c146a1f792659f0d78
broditsalera <56994255+broditsalera@users.noreply.github.com>
2019-12-06 12:55:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+//Задача 7.92
+//public class Main {
+//    public static void main(String[] args){
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите первое число: ");
+//        int a = in.nextInt();
+//        System.out.println("Введите второе число: ");
+//        int b = in.nextInt();
+//        System.out.println("Результат умножения: " + first(b,a));
+//    }
+//    public static int first(int a, int b) {
+//        if (b == 0) {
+//            return 0;
+//        } else {
+//            return a + first(b - 1, a);
+//        }
+//    }
+//}
+
+
+//Задача 7.94
+//public class Main {
+//    public static void main(String[] args){
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите число: ");
+//        int a = in.nextInt();
+//        System.out.println("Введите степень числа: ");
+//        int n = in.nextInt();
+//        System.out.println("Результат возведения в степень: " + second(n,a));
+//    }
+//    public static int second(int n, int a){
+//        if (n == 0){
+//            return 1;
+//        }
+//        else {
+//            return a * second(n - 1,a);
+//        }
+//    }
+//}
+
+//Задача 7.109
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int a = in.nextInt();
+        System.out.println("Введите второе число: ");
+        int n = in.nextInt();
+        System.out.println("Наибольший общий делитель: " + third(n,a));
+    }
+    public static int third(int n, int a){
+        if (n == 0){
+            return a;
+        }
+        else {
+            return third(a % n,n);
+        }
+    }
+}
+
